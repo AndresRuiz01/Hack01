22eac10e5e79efeb7db0bf536eb51a679a7716ac
AndresRuiz01 <46800929+AndresRuiz01@users.noreply.github.com>
2019-01-22 15:54:56 -0600

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,17 @@
+j/**
+ * Author: Teresa Gramer and Andres Ruiz
+ * Date: 2019/01/15
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Teresa and Andres\n");
+  printf("Teresa Gramer, Electrical Engineering");
+  printf("Andres Ruiz, Accounting");
+  return 0;
+}
